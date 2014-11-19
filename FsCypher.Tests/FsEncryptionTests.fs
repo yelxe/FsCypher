29f2977bdd04fdead218d4ddfd5710bafe7c71ed
yelxe <trevor.exley@gmail.com>
2014-11-18 20:48:33 -0600

    Initial commit
    Added AES encryption module, test

---
+++ FsCypher.Tests/FsEncryptionTests.fs
@@ -0,0 +1,18 @@
+﻿namespace FsCypher.Tests
+
+open NUnit.Framework
+open FsUnit
+open FsCypher.AesEncryption
+
+module ``FsEncryption Tests`` =
+    
+    module ``AES Encryption`` =
+        
+        [<Test>]
+        let ``Roundtrip should return original text`` () =
+            let algo = Aes.Create ()
+            let original = "hello"
+            let encrypted = aesEncrypt original algo.Key algo.IV
+            aesDecrypt encrypted algo.Key algo.IV |> should equal original
+           
+         
---
+++ FsCypher/aesEncryption.fs
@@ -0,0 +1,54 @@
+﻿namespace FsCypher
+
+open System.Security.Cryptography
+open System.IO
+
+module AesEncryption =
+    
+    [<AbstractClass>]
+    type Aes =  
+        class
+            inherit SymmetricAlgorithm
+        end
+    
+    let aesEncrypt (plainText:string) (key:byte[]) (iv:byte[]) =
+        
+        let algo = Aes.Create ()
+        algo.Key <- key
+        algo.IV <- iv
+
+        let encryptor = algo.CreateEncryptor(algo.Key, algo.IV)
+        
+        use msEncrypt = new MemoryStream()
+        (
+            use csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write)
+            use swEncrypt = new StreamWriter(csEncrypt)
+            swEncrypt.Write(plainText)
+        )
+        let cypherText = msEncrypt.ToArray ()
+
+        algo.Clear |> ignore
+
+        cypherText
+
+    let aesDecrypt (cypherText:byte[]) (key:byte[]) (iv:byte[]) =
+        
+        let algo = Aes.Create ()
+        algo.Key <- key
+        algo.IV <- iv
+
+        let decryptor = algo.CreateDecryptor(algo.Key, algo.IV)
+        
+        let msDecrypt = new MemoryStream(cypherText)
+        let csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read)
+        let srDecrypt = new StreamReader(csDecrypt)
+
+        let plainText = srDecrypt.ReadToEnd ()
+
+        srDecrypt.Close |> ignore
+        csDecrypt.Close |> ignore
+        msDecrypt.Close |> ignore
+
+        algo.Clear |> ignore
+
+        plainText
